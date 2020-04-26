24c0e2b9bb36b13888c721eea71c625da1b11eef
Irukan <Irukan@azet.sk>
2020-04-26 13:52:34 +0200

    Spring Initialize

---
+++ src/main/java/com/irukancocktails/IrukanCocktails/IrukanCocktailsApplication.java
@@ -0,0 +1,13 @@
+package com.irukancocktails.IrukanCocktails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrukanCocktailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrukanCocktailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irukancocktails/IrukanCocktails/IrukanCocktailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irukancocktails.IrukanCocktails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrukanCocktailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
