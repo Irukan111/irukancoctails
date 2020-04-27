1e6b5562e6722070f15335826dcc653adf0ae6d1
Irukan <mradom20@azet.sk>
2020-04-27 15:08:04 +0200

    Spring Initialize

---
+++ src/main/java/com/irukancoctails/IrukanCoctails/IrukanCoctailsApplication.java
@@ -0,0 +1,13 @@
+package com.irukancoctails.IrukanCoctails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrukanCoctailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrukanCoctailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irukancoctails/IrukanCoctails/IrukanCoctailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irukancoctails.IrukanCoctails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrukanCoctailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
